e094b7159047a3e1fe068f7c943042d4f0f8c664
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-02-10 15:29:37 -0700

    server pages

--- server.js
+++ server.js
@@ -53,30 +53,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Define a route for the root path to render the 'homepage' view
+app.get('/', (req, res) => {
+  res.render('homepage'); // This will render views/homepage.handlebars
+});
+
 const userRoutes = require('./controllers/api/userRoutes.js');
 const viewRoutes = require('./controllers/views');
 
-// Allow POST method for /api/users endpoint
-app.use(
-  '/api/users',
-  (req, res, next) => {
-    if (req.method == 'POST' || req.method == 'OPTIONS') {
-      next(); // Allow POST and OPTIONS requests
-    } else {
-      res.status(405).send('Method Not Allowed'); // Reject other methods
-    }
-  },
-  userRoutes
-);
-
 app.use('/', viewRoutes);
+
 
 sequelize
   .authenticate()
-  .then(() => console.log('Database connected.'))
+  .then(() => {
+    console.log('Database connected.');
+    // force: false is a safer option to avoid losing data
+    return sequelize.sync({ force: false });
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+  })
   .catch((err) => console.error('Unable to connect to the database:', err));
-
-// force false in production
-sequelize.sync({ force: process.env.NODE_ENV !== 'production' }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
-});
